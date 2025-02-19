11b3091c7dede0ac1b14023991e0834754c4dfdb
Tiffbroadus <broadustiffany21@gmail.com>
2025-02-19 13:43:09 -0500

    Update TB_project_01.py

--- TB_project_01.py
+++ TB_project_01.py
@@ -1,19 +1,21 @@
 print("Welcome! This program will ask you about your educational background.")
-
 name = input("What is your name? ")
-
+Tiffany 
 college = input(f"Hello {name}! Which college do you attend? ")
-
+Guttman community college
 high_school = input(f"Great! And which high school did you attend, {name}? ")
-
+urban assembly institute of math an science fo young woman 
 print(f"\nThank you for sharing, {name}! Here's a summary of your responses:")
 print(f"Name: {name}")
 print(f"College: {college}")
 print(f"High School: {high_school}")
 
-most_fun = input(f"\n{name}, which institution do you think is the most fun? ")
+most_fun = input(f"\n{name}, which institution do you think is the most fun? ") 
+high school was the most fun 
 print(f"Interesting choice! {most_fun} does sound like a lot of fun!")
 
-favorite_subject = input("What's your favorite subject? ")
-career_goal = input("What's your career goal? ")
+favorite_subject = input("What's your favorite subject? ") 
+ELA 
+career_goal = input("What's your career goal? ") 
+criminal justice an law 
 print(f"\nGreat to know that your favorite subject is {favorite_subject} and your career goal is to become a {career_goal}!")
